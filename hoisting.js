82f02f812cd1bd2cb1f9de554f47069693490e89
lucifer555-ux <visajpanchal777@gmail.com>
2024-03-07 13:24:12 +0530

    added hositing.js and memory.js to understand the behaviour of declaraction in javascript

---
+++ hoisting.js
@@ -0,0 +1,15 @@
+console.log(x); // Output: undefined
+var x = 5;
+console.log(x); // Output: 5
+
+
+sayHello(); // Output: Hello!
+
+function sayHello() {
+  console.log("Hello!");
+}
+
+
+console.log(y); // Output: ReferenceError: y is not defined
+let y = 10;
+
