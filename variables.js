c7bb1802024a4b8382813b3a5b60fc6834880f73
lucifer555-ux <visajpanchal777@gmail.com>
2024-03-07 06:57:59 +0530

    added two more methods :  let and const

--- variables.js
+++ variables.js
@@ -1,11 +1,27 @@
 function test() {
 var a = 5;
 console.log(a);
- a  =6;
+ a  =6;         // Global variable
 console.log(a);
 
 }
 test();
 a = 9;
 console.log(a);
+
+let name = "Visaj";
+
+console.log("Printing outside :  " + name);
+
+function printName() {
+    console.log("Printing inside :  " + name); // Cannot access inside function
+}
+
+const num = 10;
+
+console.log(num);
+
+num = 20;
+
+console.log(num); // TypeError: Assignment to constant variable.
 
