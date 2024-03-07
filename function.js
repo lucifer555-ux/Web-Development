17cc17d0cde5b4db1f42499f3b91f2c5bb095b65
lucifer555-ux <visajpanchal777@gmail.com>
2024-03-07 16:00:26 +0530

    added map function, higher order function and arrow function

--- function.js
+++ function.js
@@ -1,17 +1,17 @@
-// function myfunction()
-// {
-//     console.log("Hello Visaj");
-// }
+ function myfunction()
+ {
+     console.log("Hello Visaj");
+ }
 
-// myfunction();
+ myfunction();
 
 
-// // Multiplication
+// Multiplication
 
-// function mul(a,b){
-//     console.log(a + b);
-// }
-// mul(231,6);
+ function mul(a,b){
+     console.log(a + b);
+ }
+mul(231,6);
 
 const Addition = (a, b) => {
     return a + b;
@@ -19,4 +19,95 @@ const Addition = (a, b) => {
 
 Addition(9,10);
 
+let greet = function () {
+
+  console.log("Hello world");
+  return 1;
+  
+}
+
+
+console.log(greet());
+
+
+// let mult = (a,b) => {
+
+//   return a * b;
+// }
+
+let mult = (a,b) => (a * b);
+
+
+
+console.log("Arrow function " + mult(55,10));
+
+function countVowels(str){
+  let count = 0;
+  for(const char of str)
+  {
+    char === "a" || char === "e" || char === "i" || char === "o" || char === "u"
+    {
+      count++;
+    }
+  }
+  console.log(count); 
+}
+
+let array = [10,20,30];
+
+array.forEach((n)=>{
+  console.log(n * 20);
+})
+console.log(array);
+
+
+const radius = [5,2,4,9];
+
+const calculateArea = function(radius) {
+  const output = [];
+  for (let i = 0; i < radius.length; i++) {
+      output.push(Math.PI * radius[i] * radius[i]);
+  }
+  return output;
+};
+
+console.log(calculateArea(radius));
+
+
+const nums = [2,4,8,10];
+console.log("Square ");
+
+nums.forEach((n)=>{
+  console.log(n * n);
+})
+
+const a1 = [100,200,300,400,500];
+
+function double (x)
+{
+  return x * 2;
+}
+
+const output = a1.map(double);
+
+console.log("Double : " + output);
+
+
+function triple (x)
+{
+  return x * 3;
+}
+
+const op = a1.map(triple);
+
+console.log("Triple : " + op);
+
+function binary (x)
+{
+  return x.toString(2);
+}
+
+const o = a1.map(binary);
+
+console.log("Binary : " + o);
 
