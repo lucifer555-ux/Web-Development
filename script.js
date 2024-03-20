57a6e728408291f85bd6c02d4d9123167c87f50c
lucifer555-ux <visajpanchal777@gmail.com>
2024-03-20 09:45:57 +0530

    learned about javascript DOM

---
+++ script.js
@@ -0,0 +1,42 @@
+// let button = document.getElementById("#btnID");
+
+// console.log(button)
+
+// let para = document.getElementById("paraID");
+
+// console.log(para)
+
+// let headings = document.getElementsByClassName("myClass");
+
+// console.log(headings)
+
+
+// // Query Selector
+
+// let elements = document.querySelector("p");
+
+// console.log(elements)
+
+// Challenge
+
+// let h2 = document.querySelector("h2");
+
+// console.dir(h2.innerText);
+
+// h2.innerText = h2.innerText + " from Apna College ";
+
+// let divs = document.querySelectorAll(".box")
+
+// divs[0].innerText ="JAWAN"
+
+// console.log(divs[0]);
+
+
+
+// let newBtn = document.createElement("button");
+// newBtn.innerText = "Click Me !!";
+
+// newBtn.style.color = "white";
+// newBtn.style.backgroundColor = "blue"; // Corrected line
+
+// document.querySelector("body").prepend(newBtn);
